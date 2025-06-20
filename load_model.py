4a97480dd377e3eedb9881e7dfa65e5d858deade
Pranava Bhat <72594385+pranavabhat-5204@users.noreply.github.com>
2025-06-20 16:53:54 +0530

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -1,19 +1,20 @@
-data= {'title':ds['title'],'description':ds['title']}
-df=pd.DataFrame(data)
-tokenized_dataset1=dataset['title'].map()
-model = Sequential() 
-model.add(LSTM(128, input_shape =(max_length, len(vocabulary)))) 
-model.add(Dense(len(vocabulary))) 
-model.add(Activation('softmax')) 
-optimizer = RMSprop(lr = 0.01) 
-model.compile(loss ='categorical_crossentropy', optimizer = optimizer) 
+from transformers import pipeline, set_seed
 
-model = Sequential() 
-model.add(LSTM(128, input_shape =(567, 30)))
-model.add(Dense(len(vocabulary))) 
-model.add(Activation('softmax')) 
-model.compile(loss ='categorical_crossentropy', optimizer = 'adam') 
+generator = pipeline('text-generation', model="./gpt2-title-desc", tokenizer=tokenizer)
+set_seed(42)
 
-model.fit(X1, y1, batch_size = 128, epochs = 500) 
-model.save('gfgModel.h5')
-X1=X1.reshape(None,567,30)
+title = "Best laptops"
+output = generator(title, max_length=100, num_return_sequences=1)
+print(output[0]['generated_text'])
+
+# to load the RAG system based fine tuned model
+from transformers import pipeline
+
+generator = pipeline('text-generation', model="./gpt2-title-desc", tokenizer=tokenizer)
+
+def generate_description(title):
+    prompt = retrieve_context(title,0.3)
+    prompt = f"Title:{title} \nDescription:{prompt}"
+    output = generator(prompt, temperature=0.7, top_k=50, top_p=0.9, do_sample=True, max_length=100, num_return_sequences=1)
+    return output[0]["generated_text"]
+generate_description('HP laptop')
