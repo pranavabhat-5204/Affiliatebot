9c30a289cb368f0b0b689880a565407599d8168e
Pranava Bhat <72594385+pranavabhat-5204@users.noreply.github.com>
2025-07-24 20:05:40 +0530

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -12,11 +12,4 @@ print(output[0]['generated_text'])
 # to load the RAG system based fine tuned model
 from transformers import pipeline
 
-generator = pipeline('text-generation', model="./gpt2-title-desc", tokenizer=tokenizer)
-
-def generate_description(title):
-    prompt = retrieve_context(title,0.3)
-    prompt = f"Title:{title} \nDescription:{prompt}"
-    output = generator(prompt, temperature=0.7, top_k=50, top_p=0.9, do_sample=True, max_length=100, num_return_sequences=1)
-    return output[0]["generated_text"]
 generate_description('HP laptop')
